da446e031aa1a9263b6b52bce873de792b82bfa0
Arto Bendiken <arto@bendiken.net>
2009-12-22 01:44:05 +0100

    Implemented synchronous service discovery.

--- lib/adhoc/dnssd.rb
+++ lib/adhoc/dnssd.rb
@@ -5,9 +5,21 @@ module Adhoc
   # DNS-SD service discovery.
   module DNSSD
     ##
-    # Discovers services of a given type using DNS-SD.
-    def self.discover(type, &block)
-      service = Service.for(type)
+    # Discovers DNS-SD services synchronously.
+    def self.discover!(service, options = {}, &block)
+      query = discover(service, options, &block)
+      if options[:timeout]
+        sleep options[:timeout].to_f
+      else
+        sleep # wait indefinitely
+      end
+      query.stop
+    end
+
+    ##
+    # Discovers DNS-SD services asynchronously.
+    def self.discover(service, options = {}, &block)
+      service = Service.for(service)
       ::DNSSD.browse(service.type) do |reply|
         ::DNSSD.resolve(reply) do |reply|
           block.call(service.new(reply))
