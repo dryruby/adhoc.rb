d76d68d737488c5961eb0e34f1167e4033c4b97e
Arto Bendiken <arto@bendiken.net>
2009-12-22 01:44:58 +0100

    Implemented Adhoc.discover! and Adhoc.discover.

--- lib/adhoc.rb
+++ lib/adhoc.rb
@@ -6,4 +6,16 @@ module Adhoc
   autoload :AMQP,    'adhoc/amqp'
   autoload :DNSSD,   'adhoc/dnssd'
   autoload :XMPP,    'adhoc/xmpp'
+
+  ##
+  # Discovers services synchronously.
+  def self.discover!(service, options = {}, &block)
+    DNSSD.discover!(service, options, &block)
+  end
+
+  ##
+  # Discovers services asynchronously.
+  def self.discover(service, options = {}, &block)
+    DNSSD.discover(service, options, &block)
+  end
 end
