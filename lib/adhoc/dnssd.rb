dfcf488a0bea64c1c7e51720dbf5780e363b888f
Arto Bendiken <arto@bendiken.net>
2009-12-22 01:16:02 +0100

    Implemented DNS-SD service discovery.

--- lib/adhoc/dnssd.rb
+++ lib/adhoc/dnssd.rb
@@ -1,7 +1,18 @@
 require 'dnssd'
 
 module Adhoc
+  ##
+  # DNS-SD service discovery.
   module DNSSD
-    # TODO
+    ##
+    # Discovers services of a given type using DNS-SD.
+    def self.discover(type, &block)
+      service = Service.for(type)
+      ::DNSSD.browse(service.type) do |reply|
+        ::DNSSD.resolve(reply) do |reply|
+          block.call(service.new(reply))
+        end
+      end
+    end
   end
 end
